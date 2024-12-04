d5eda4cdce714f8b4d0c3e359d5540b96dff2258
ikavodo <ido.akov1@alumni.aalto.fi>
2024-12-04 21:49:14 +0100

    first commit

--- matlab-utils/bootstrap.m
+++ matlab-utils/bootstrap.m
@@ -1,4 +1,4 @@
-%helper for sampling some num of samples from existing channels of signal
+%helper for sampling some num of samples from existing channels
 function bootstrapped = bootstrap(in_channels, num_samples)
     X = randi(size(in_channels, 2), size(in_channels, 1), num_samples);
     indices = sub2ind(size(in_channels), repmat(1:size(in_channels, 1), num_samples, 1)', X);
